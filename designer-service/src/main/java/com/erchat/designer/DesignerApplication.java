f240fd8d2043930ad4a3aa50de233a44989196a1
qinoy <qinoy8521@163.com>
2024-11-06 17:25:13 +0800

    designer-service init

---
+++ designer-service/src/main/java/com/erchat/designer/DesignerApplication.java
@@ -0,0 +1,16 @@
+package com.erchat.designer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author oYang
+ * @Description TODO
+ * @createTime 2024年11月06日 16:14:00
+ */
+@SpringBootApplication
+public class DesignerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DesignerApplication.class, args);
+	}
+}
---
+++ designer-service/src/main/java/com/erchat/designer/service/IDesignerService.java
@@ -0,0 +1,12 @@
+package com.erchat.designer.service;
+
+/**
+ * @author oYang
+ * @Description 设计器服务
+ * @createTime 2024年11月06日 17:04:00
+ */
+public interface IDesignerService {
+
+	void save();
+
+}
