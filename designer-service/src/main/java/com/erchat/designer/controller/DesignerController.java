1bdf24ab696fa2ed061182e5f311a13faa1d793b
qinoy <qinoy8521@163.com>
2024-11-25 17:30:51 +0800

    🐟

--- designer-service/src/main/java/com/erchat/designer/controller/DesignerController.java
+++ designer-service/src/main/java/com/erchat/designer/controller/DesignerController.java
@@ -1,33 +1,34 @@
 package com.erchat.designer.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.erchat.common.domain.APIResponse;
 import com.erchat.designer.model.ERDiagram;
 import com.erchat.designer.service.IDesignerService;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author oYang
  * @Description 设计器控制层
  * @createTime 2024年11月11日 11:18:00
  */
-@Controller
+@RestController
 @RequestMapping("/designer")
+@RequiredArgsConstructor
 public class DesignerController {
 
-	@Autowired
-	private IDesignerService designerService;
+	private final IDesignerService designerService;
 
 	@PostMapping("/save")
-	public ResponseEntity save(@RequestBody ERDiagram erDiagram){
+	public APIResponse<Object> save(@RequestBody ERDiagram erDiagram){
 		designerService.save(erDiagram);
 
-		return ResponseEntity.ok().build();
+		return APIResponse.success("保存成功");
 	}
 }
